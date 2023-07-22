52fa6f04be314229e3effa140557f6448b51a01f
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-07-22 20:53:19 -0300

    Adicionar parâmetro 'print_info' para controle de informações adicionais

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ def rodando_checkagem_de_precos():
     url_pesquisa = f"https://www.amazon.com.br/s?k={termo_de_pesquisa.replace(' ', '+')}"
 
     total_paginas = obter_numero_total_paginas(url_pesquisa)
-    lista_urls_paginas = paginas.obter_lista_urls_paginas(url_pesquisa, total_paginas)
-    for i, url_pagina in enumerate(lista_urls_paginas, 1):
-        print(f"Verificando página {i} de {total_paginas}...")
+    numero_da_pagina = paginas.obter_lista_urls_paginas(url_pesquisa, total_paginas, print_info=True)
 
-        time.sleep(1)
     # while True:
     #     url = "https://www.amazon.com.br/Monitor-AOC-G-Sync-Compatible-27G2/dp/B088L3TM7X/ref=sr_1_1?pf_rd_i=16364756011&pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_p=3a4566d3-19f5-4330-ba47-df1b7bbd88a2&pf_rd_r=TBJX7096TP23B8K7MYV4&pf_rd_s=merchandised-search-3&pf_rd_t=101&qid=1689553216&s=computers&sr=1-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147"
     #     check_price(url)
